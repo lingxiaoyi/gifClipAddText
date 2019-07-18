8558ae0978c9b1f07fe44dd28d8736332a776ccc
zhijun <550843086@qq.com>
2019-07-18 14:08:33 +0800

    字段限制 分段

--- js/gifmain.js
+++ js/gifmain.js
@@ -6,7 +6,7 @@ var view = document.getElementById('view');
 var drop = document.getElementById('drop');
 
 var canvas_sprite = new fabric.Canvas('merge');
-
+let TextPosition = ['', '', '']; //所有文字元素的信息
 document.getElementById('import-gif').addEventListener('change', function(e) {
   var file = e.target.files[0];
 
@@ -74,14 +74,13 @@ function loadBuffer(file, onload, onerror, onprogress) {
   fr.readAsArrayBuffer(file);
 }
 
+let $select = $('#select');
+
 function buildView(gif, fname, preRender) {
   var canvas_frame, context, frames;
   let imgWidth = '';
   let imgHeight = '';
-  let t = '';
-  let rects = [];
-  let texts = ['', '', ''];
-  let textsPosition = [];
+
   canvas_frame = document.createElement('canvas');
   canvas_frame.width = gif.header.width;
   canvas_frame.height = gif.header.height;
@@ -89,7 +88,7 @@ function buildView(gif, fname, preRender) {
   context = canvas_frame.getContext('2d');
   frames = gif.createFrameImages(context, preRender, !preRender);
   gif_frames = frames.length;
-
+  canvas_sprite.clear();
   frames.forEach(function(frame, i) {
     var canvas_frame;
     canvas_frame = document.createElement('canvas');
@@ -112,166 +111,300 @@ function buildView(gif, fname, preRender) {
         ScalingRatio = 1 /* canvas_sprite_width / TotalWidth */; //缩放比 画布大小/图片真实大小
         imgWidth = img.getWidth();
         imgHeight = img.getHeight();
-        img.set({ selectable: false });
+        img.set({ selectable: false, fill: '#000000' });
         img.left = img.getWidth() * i;
         width = img.getWidth() * i + 1;
         canvas_sprite.setHeight(img.getHeight());
         canvas_sprite.setWidth(img.getWidth() * (i + 1)); //画布大小固定成800
         canvas_sprite.add(img);
+        //加线进来
+        let Line = new fabric.Line([img.getWidth() * i, 0, img.getWidth() * i, img.getHeight()], {
+          selectable: false,
+          fill: '#000000',
+          stroke: 'rgba(0,0,0,0.8)' //笔触颜色
+        });
+        canvas_sprite.add(Line);
         canvas_sprite.renderAll();
         if (i == frames.length - 1) {
           $('#view').append('<img src="' + canvas_sprite.toDataURL('png') + '">');
         }
         //假如自定义的图形 加载完最后一帧图片再
         if (i === frames.length - 1) {
-          renderFrames();
+          $select.val('3');
+          $select.change();
         }
       });
     } else {
       alert('无效的GIF');
     }
   });
-  let length = frames.length;
-  let residue = length % 3;
-  let average = (length - residue) / 3; //整除值
-  let framesArr = [average, average + residue, average];
-  let $inputNum = $('.option .item .num');
-  let $inputText = $('.option .item .giftext');
-  let $add = $('.option .item .add');
-  let $reduce = $('.option .item .reduce');
-
-  $add.click(function() {
-    let framesArrIndex = $(this)
-      .prev()
-      .data('framesArrIndex');
-
-    if (framesArrIndex === framesArr.length - 1) {
-      if (framesArr[0] === 1) {
-        alert('不能再加了已经最大了');
-        return;
+  let framesArr = [];
+  let rects = [];
+  let texts = [];
+  $select.unbind('change');
+  $select.change(function() {
+    let clipPartNum = $(this).val();
+    let length = frames.length;
+    let residue = length % clipPartNum;
+    let average = (length - residue) / clipPartNum; //整除值
+    framesArr = []; //将每段的帧数放进数组
+    let t = '';
+    TextPosition = [];
+    $('.option').html('');
+    rects.forEach(function(item) {
+      canvas_sprite.remove(item);
+    });
+    texts.forEach(function(item) {
+      canvas_sprite.remove(item);
+    });
+    rects = [];
+    texts = [];
+    for (let index = 0; index < clipPartNum; index++) {
+      if (index === 1) {
+        framesArr.push(average + residue);
+      } else {
+        framesArr.push(average);
       }
-      framesArr[0]--;
-    } else {
-      if (framesArr[framesArrIndex + 1] === 1) {
-        alert('不能再加了已经最大了');
-        return;
-      }
-      framesArr[framesArrIndex + 1]--;
+      $('.option').append(`<div class="item">
+        <div class="n">第${index + 1}段</div>
+        <input class="num" type="text" />
+        <div class="add">+</div>
+        <div class="reduce">-</div>
+        <input class="giftext" type="text" value="文字测试${index + 1}" />
+        <input class="textNumMax" type="text" value="4" />
+        <input class="textNumLimit" type="text" value="4" />
+        <label>是否填充文字：</label>
+        是<input type="radio" name="radio${index + 1}" value="1" checked="checked"/>
+        否<input type="radio" name="radio${index + 1}" value="0"/>
+      </div>`);
     }
+    let $inputNum = $('.option .item .num');
+    let $inputText = $('.option .item .giftext');
+    let $add = $('.option .item .add');
+    let $reduce = $('.option .item .reduce');
+    let $textNumMax = $('.option .item .textNumMax');
+    let $textNumLimit = $('.option .item .textNumLimit');
+    let $inputRadio = $('.option .item input:radio');
+    console.log('framesArr', framesArr);
+    $textNumMax.change(function(params) {
+      renderFrames();
+    });
+    $textNumLimit.change(function(params) {
+      renderFrames();
+    });
+    $inputRadio.click(function(params) {
+      /* $(this)
+        .parent()
+        .find('input:radio')
+        .attr('checked', false);
+      $(this).attr('checked', 'checked'); */
+      renderFrames();
+    });
+    $add.unbind('click');
+    $reduce.unbind('click');
+    $add.click(function() {
+      let framesArrIndex = $(this)
+        .prev()
+        .data('framesArrIndex');
 
-    framesArr[framesArrIndex]++;
+      if (framesArrIndex === framesArr.length - 1) {
+        if (framesArr[0] === 1) {
+          alert('不能再加了已经最大了');
+          return;
+        }
+        framesArr[0]--;
+      } else {
+        if (framesArr[framesArrIndex + 1] === 1) {
+          alert('不能再加了已经最大了');
+          return;
+        }
+        framesArr[framesArrIndex + 1]--;
+      }
+
+      framesArr[framesArrIndex]++;
+      renderFrames();
+    });
+    $reduce.click(function() {
+      let framesArrIndex = $(this)
+        .prev()
+        .prev()
+        .data('framesArrIndex');
+      if (framesArr[framesArrIndex] === 1) {
+        alert('不能再减了已经最小了');
+        return;
+      }
+      framesArr[framesArrIndex]--;
+      if (framesArrIndex === framesArr.length - 1) {
+        framesArr[0]++;
+      } else {
+        framesArr[framesArrIndex + 1]++;
+      }
+
+      renderFrames();
+    });
+
     renderFrames();
+    function renderFrames() {
+      $inputNum.each(function(i) {
+        let $that = $(this);
+        $(this).val(framesArr[i]);
+        $(this).data('framesArrIndex', i);
+        if (rects.length) {
+          rects.forEach(function(item, i) {
+            let left = 0;
+            for (let index = 0; index < i; index++) {
+              left += framesArr[index] * imgWidth;
+            }
+            item.set({
+              left: left,
+              width: imgWidth * framesArr[i], //方形的宽度
+              height: imgHeight //方形的高度
+            });
+          });
+          /* texts.forEach(function(item, i) {
+            let left = 0;
+            for (let index = 0; index < i; index++) {
+              left += framesArr[index] * imgWidth;
+            }
+            item.set({
+              left: left
+            });
+            TextPosition[i] = {
+              left,
+              top: 0,
+              textWidth: texts[i].width,
+              textHeight: texts[i].height,
+              fontSize: 14,
+              frames: framesArr[i],
+              imgWidth,
+              imgHeight
+            };
+          }); */
+          TextPosition[i] = {
+            ...TextPosition[i],
+            textNumMax: $that
+              .parent()
+              .find('.textNumMax')
+              .val(),
+            textNumMin: $that
+              .parent()
+              .find('.textNumLimit')
+              .val(),
+            isAddText: !!(
+              $that
+                .parent()
+                .find(`input[name=radio${i + 1}]:checked`)
+                .val() / 1
+            )
+          };
+          canvas_sprite.renderAll();
+        } else {
+          let bgColor = ['rgba(0,0,0,0.3)', 'rgb(4, 250, 37, 0.3)', 'rgb(41, 4, 250, .3)', 'rgb(41, 4, 10, .3)'];
+          t = setTimeout(() => {
+            let left = 0;
+            for (let index = 0; index < i; index++) {
+              left += framesArr[index] * imgWidth;
+            }
+            let rect = new fabric.Rect({
+              left: left, //距离画布左侧的距离，单位是像素
+              top: 0, //距离画布上边的距离
+              fill: bgColor[i], //填充的颜色
+              width: imgWidth * framesArr[i], //方形的宽度
+              height: imgHeight, //方形的高度
+              selectable: false
+            });
+            rects.push(rect);
+            canvas_sprite.add(rect);
+            //加文字进去
+            texts[i] = new fabric.Text($inputText.eq(i).val(), {
+              left: left, //距离画布左侧的距离，单位是像素
+              top: 0, //距离画布上边的距离
+              fontSize: 14, //文字大小
+              lockRotation: true,
+              index: i
+            });
+            TextPosition[i] = {
+              left,
+              top: 0,
+              textWidth: texts[i].width,
+              textHeight: texts[i].height,
+              fontSize: 14,
+              frames: framesArr[i],
+              imgWidth,
+              imgHeight,
+              textNumMax: $that
+                .parent()
+                .find('.textNumMax')
+                .val(),
+              textNumMin: $that
+                .parent()
+                .find('.textNumLimit')
+                .val(),
+              isAddText: !!(
+                $that
+                  .parent()
+                  .find(`input[name=radio${i + 1}]:checked`)
+                  .val() / 1
+              )
+            };
+            //texts.push(Text);
+            canvas_sprite.add(texts[i]);
+            //clearTimeout(t);
+          }, 10);
+        }
+      });
+    }
   });
-  $reduce.click(function() {
-    let framesArrIndex = $(this)
-      .prev()
-      .prev()
-      .data('framesArrIndex');
-    if (framesArr[framesArrIndex] === 1) {
-      alert('不能再减了已经最小了');
+
+  canvas_sprite.on('object:moving', function(e) {
+    //console.log('moving a rectangle', e);
+    let $inputText = $('.option .item .giftext');
+    let arrText = [];
+    $inputText.each(function(i) {
+      arrText.push($(this).val());
+    });
+    var obj = e.target;
+    // if object is too big ignore
+    if (obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width) {
       return;
     }
-    framesArr[framesArrIndex]--;
-    if (framesArrIndex === framesArr.length - 1) {
-      framesArr[0]++;
-    } else {
-      framesArr[framesArrIndex + 1]++;
+    obj.setCoords();
+    // top-left  corner
+    if (obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0) {
+      obj.top = Math.max(obj.top, obj.top - obj.getBoundingRect().top);
+      obj.left = Math.max(obj.left, obj.left - obj.getBoundingRect().left);
     }
-
-    renderFrames();
-  });
-
-  function renderFrames() {
-    $inputNum.each(function(i) {
-      $(this).val(framesArr[i]);
-      $(this).data('framesArrIndex', i);
-      console.log('i', i);
-      if (rects.length) {
-        rects.forEach(function(item, i) {
-          let left = 0;
-          for (let index = 0; index < i; index++) {
-            left += framesArr[index] * imgWidth;
-          }
-          item.set({
-            left: left,
-            width: imgWidth * framesArr[i], //方形的宽度
-            height: imgHeight //方形的高度
-          });
-        });
-        texts.forEach(function(item, i) {
-          let left = 0;
-          for (let index = 0; index < i; index++) {
-            left += framesArr[index] * imgWidth;
-          }
-          item.set({
-            left: left,
-            width: imgWidth * framesArr[i], //方形的宽度
-            height: imgHeight //方形的高度
-          });
-        });
-        canvas_sprite.renderAll();
-      } else {
-        let bgColor = ['rgba(0,0,0,0.1)', 'rgb(4, 250, 37, 0.1)', 'rgb(41, 4, 250, .1)'];
-        t = setTimeout(() => {
-          let left = 0;
-          for (let index = 0; index < i; index++) {
-            left += framesArr[index] * imgWidth;
-          }
-          let rect = new fabric.Rect({
-            left: left, //距离画布左侧的距离，单位是像素
-            top: 0, //距离画布上边的距离
-            fill: bgColor[i], //填充的颜色
-            width: imgWidth * framesArr[i], //方形的宽度
-            height: imgHeight, //方形的高度
-            selectable: false
-          });
-          rects.push(rect);
-          canvas_sprite.add(rect);
-          //加文字进去
-          texts[i] = new fabric.Text($inputText.eq(i).val(), {
-            left: left, //距离画布左侧的距离，单位是像素
-            top: 0, //距离画布上边的距离
-            fontSize: 14, //文字大小
-            lockRotation: true,
-            index: i
-          });
-          //texts.push(Text);
-          canvas_sprite.add(texts[i]);
-          //clearTimeout(t);
-        }, 10);
-      }
+    // bot-right corner
+    if (
+      obj.getBoundingRect().top + obj.getBoundingRect().height > obj.canvas.height ||
+      obj.getBoundingRect().left + obj.getBoundingRect().width > obj.canvas.width
+    ) {
+      obj.top = Math.min(
+        obj.top,
+        obj.canvas.height - obj.getBoundingRect().height + obj.top - obj.getBoundingRect().top
+      );
+      obj.left = Math.min(
+        obj.left,
+        obj.canvas.width - obj.getBoundingRect().width + obj.left - obj.getBoundingRect().left
+      );
+    }
+    let { top, left, width, height, fontSize } = e.target;
+    let index = arrText.findIndex(function(text) {
+      return text === e.target.text;
     });
-  }
+    TextPosition[index] = {
+      left,
+      top,
+      textWidth: width,
+      textHeight: height,
+      fontSize,
+      frames: framesArr[index],
+      imgWidth,
+      imgHeight
+    };
+  });
 }
-canvas_sprite.on('object:moving', function(e) {
-  console.log('moving a rectangle', e);
-  var obj = e.target;
-  // if object is too big ignore
-  if (obj.currentHeight > obj.canvas.height || obj.currentWidth > obj.canvas.width) {
-    return;
-  }
-  obj.setCoords();
-  // top-left  corner
-  if (obj.getBoundingRect().top < 0 || obj.getBoundingRect().left < 0) {
-    obj.top = Math.max(obj.top, obj.top - obj.getBoundingRect().top);
-    obj.left = Math.max(obj.left, obj.left - obj.getBoundingRect().left);
-  }
-  // bot-right corner
-  if (
-    obj.getBoundingRect().top + obj.getBoundingRect().height > obj.canvas.height ||
-    obj.getBoundingRect().left + obj.getBoundingRect().width > obj.canvas.width
-  ) {
-    obj.top = Math.min(
-      obj.top,
-      obj.canvas.height - obj.getBoundingRect().height + obj.top - obj.getBoundingRect().top
-    );
-    obj.left = Math.min(
-      obj.left,
-      obj.canvas.width - obj.getBoundingRect().width + obj.left - obj.getBoundingRect().left
-    );
-  }
-  let { top, left, width, height } = e.target;
-  reactPosition = { left, top, width, height };
 
-});
-
+document.getElementById('btn').onclick = function() {
+  console.log('TextPosition', TextPosition);
+};
